637869ba59cda50e8be63a301e69b14860d6ebf1
Gustavo Henrique Okuda <165102258+gustavookuda@users.noreply.github.com>
2025-04-20 21:22:46 -0300

    Indentação
    Indentei essas porra e add o bootstrap

--- cabecalho.php
+++ cabecalho.php
@@ -1,19 +1,20 @@
 <!DOCTYPE html>
 <html lang="pt-br">
 <head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel='stylesheet' href='index.css'>
-    <link rel="stylesheet" href="node_modules/bootstrap/dist/css">
-    <title>Login</title>
-    
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.5/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-SgOJa3DmI69IUzQ2PVdRZhwQ+dy64/BUtbMJw1MZ8t5HZApcHrRKUc4W0kG879m7" crossorigin="anonymous">
+  <link rel='stylesheet' href='index.css'>
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.5/dist/js/bootstrap.bundle.min.js" integrity="sha384-k6d4wzSIapyDyv1kpU366/PK5hCdSbCRGRCMv+eplOQJWyd1fbcAu9OCUj5zNLiq" crossorigin="anonymous"></script>
+  <title>Login</title>
 </head>
+
 <body>
 <nav class="nav nav-pills flex-column flex-sm-row">
-            <a class="flex-sm-fill text-sm-center nav-link" href="index.php">Inicio</a>
-            <a class="flex-sm-fill text-sm-center nav-link" href="Time/Times.php">Times</a>
-            <a class="flex-sm-fill text-sm-center nav-link" href="principal.php">Lista de Cadastros</a>
-            <a class="flex-sm-fill text-sm-center nav-link" href="login.php">Entrar</a>
-            <a class="flex-sm-fill text-sm-center nav-link" href="Quiz/index.php">QUIZ</a>
-          </nav>
+  <a class="flex-sm-fill text-sm-center nav-link" href="index.php">Inicio</a>
+  <a class="flex-sm-fill text-sm-center nav-link" href="Time/Times.php">Times</a>
+  <a class="flex-sm-fill text-sm-center nav-link" href="principal.php">Lista de Cadastros</a>
+  <a class="flex-sm-fill text-sm-center nav-link" href="login.php">Entrar</a>
+  <a class="flex-sm-fill text-sm-center nav-link" href="Quiz/index.php">QUIZ</a>
+</nav>
--- index.php
+++ index.php
@@ -1,7 +1,3 @@
-<?php
-include('cabecalho.php')
-?>
-<h1>Site de n sei o que n sei o que lá</h1>
-<?php
-include('rodape.php')
-?>
+<?php include('cabecalho.php') ?>
+    <h1>Site de n sei o que n sei o que lá</h1>
+<?php include('rodape.php') ?>
